9db7de2ccb3f6bfc6ab40587e5a7e60b6151a4cd
ysuresh81@gmail.com <s.satya.yellamelli@accenture.com>
2017-09-27 09:36:51 +0530

    Signed-off-by: ysuresh81@gmail.com <s.satya.yellamelli@accenture.com>

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 var express = require('express');
 var app = express();
-
 app.use(express.static('public'));
-app.use(express.static('src/views'));
+app.set('views','./src/views');
+app.set('view engine', 'ejs');
+
+app.use('/',function(req,res){
+    res.render('index',{ list: ['a','c']});
+});
 var port = process.env.port || 5000;
 app.listen(port, (err) => {    
     if(! err){
