7b62f1ef0774399699711ecde707fa6a9a3bf74f
Suresh Yellamelli <ysuresh81@gmail.com>
2017-10-05 08:22:42 +0530

    Signed-off-by: Suresh Yellamelli <ysuresh81@gmail.com>

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
+var passport = require('passport');
+var session = require('express-session');
+
+
 var app = express();
 var port = process.env.PORT || 5000;
 var nav = [{
@@ -13,10 +18,15 @@ var bookRouter = require('./src/routes/bookRoutes')(nav);
 var adminRouter = require('./src/routes/adminRoutes')(nav);
 var authRouter = require('./src/routes/authRoutes')(nav);
 
+//middleware
 app.use(express.static('public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
+app.use(cookieParser());
+app.use(session({secret: 'library'}));
+require('./src/config/passport')(app);
 
+//middleware
 app.set('views', './src/views');
 app.set('view engine', 'ejs');
 app.use('/Books', bookRouter);
